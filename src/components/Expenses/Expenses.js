c4a49cf6643ba8e702a8c3146024a38566f01073
raunaqk1202 <raunaqk1202@gmail.com>
2023-08-10 19:21:11 +0530

    Assignment-2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,16 +2,17 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props){
+   const[filteredYear,setFilteredYear] = useState('2019');
    const handleSelectedYear = (selectedYear) =>{
-      console.log("In Expenses.js");
-      console.log(selectedYear);
+     setFilteredYear(selectedYear);
    }
      return(
        <div>
-        <ExpensesFilter onSelectedYear={handleSelectedYear}/>
         <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSelectedYear={handleSelectedYear}/>
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
